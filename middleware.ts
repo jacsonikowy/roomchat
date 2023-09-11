9f9c6bdaac586f354db68acb56fce95239e09f1d
jacsonikowy <michal6345@wp.pl>
2023-09-11 21:32:28 +0200

    fix: while login no access to /

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  ignoredRoutes: "/",
+  afterAuth(auth, req) {
+    if (!auth.userId) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/";
+      return NextResponse.rewrite(url);
+    }
+  },
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)", "/chat"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
